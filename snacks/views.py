6d30fbb21c68c506f357e20a0f67633605a85095
moradalkhatib <muradalkhateb@icloud.com>
2021-11-15 19:46:52 +0200

    make app for snacks

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView ,DetailView
+from snacks.models import Snack
 
-# Create your views here.
+
+class HomeView(ListView):
+    template_name = 'home.html'
+    model = Snack
+
+
+class SnackListDetail(DetailView):
+    template_name = "list_detail.html"
+    model = Snack
+    context_object_name = "snack_object"
